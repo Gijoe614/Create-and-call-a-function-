28f7b5ff899c1726adc87f1d979a2126d2920a15
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-09-07 14:12:25 -0500

    Add files via upload

---
+++ Create and call a function.py
@@ -0,0 +1,9 @@
+def sing_childsprayer():
+    print("Hush my dear!")
+    print("Lie still and slumber!")
+    print("Holy angels guard thy bed,")
+    print("Heavenly blessings, without number,")
+    print("Gently falling on thy head.")
+
+sing_childsprayer()
+sing_childsprayer()
